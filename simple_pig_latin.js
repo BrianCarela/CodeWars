d5369eea880a1fe5043abe517168e19df608b2c5
Brian Carela <BrianCarela@gmail.com>
2018-09-11 14:39:19 -0400

    solved for solo punctuation using regex

--- simple_pig_latin.js
+++ simple_pig_latin.js
@@ -6,7 +6,20 @@ function pigIt(str){
   var arr = str.split(" ");
 
   for(var i = 0; i<arr.length; i++){
-    arr[i] = arr[i].slice(1) + arr[i].split("").shift() + "ay";
+    // console.log(arr[i].search(/([.!?])/));
+
+    let firstChar = arr[i].split("").shift();
+    let noPunct = arr[i].slice(1).replace(/([.!?])/,"");
+    let punctInd = arr[i].search(/([.!?])/);
+
+    if(0 == punctInd){
+      // nothing happens, leave punctuation alone
+    } else if(punctInd >=0){
+      arr[i] = noPunct + firstChar + "ay" + arr[i][punctInd];
+    } else{
+      arr[i] = noPunct + firstChar + "ay";
+    }
+
   }
 
   return arr.join(" ");
